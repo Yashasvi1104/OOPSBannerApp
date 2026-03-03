e5b9a74f744678822a58c079070a90f69ee1d7a8
Yashasvi1104 <yashasviparihar1104@gmail.com>
2026-03-03 13:17:38 +0530

    uc4 done

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,17 @@
-class OOPSBannerApp3 {
+class OOPSBannerApp4 {
+
     public static void main(String[] args) {
 
-        System.out.println(String.join("", "      ***      ***      *****       ***** "));
-        System.out.println(String.join("", "     ** **    ** **    **   **    **      "));
-        System.out.println(String.join("", "    **   **  **   **   **   **   **       "));
-        System.out.println(String.join("", "    **   **  **   **   *****      ***     "));
-        System.out.println(String.join("", "    **   **  **   **   **            **   "));
-        System.out.println(String.join("", "     ** **    ** **    **             **  "));
-        System.out.println(String.join("", "      ***      ***     **        *****    "));
+        String[] banner = {
+                String.join("", "  OOO   ", "  OOO   ", "  PPPP   ", "  SSSS "),
+                String.join("", " O   O  ", " O   O  ", "  P   P  ", " S     "),
+                String.join("", " O   O  ", " O   O  ", "  PPPP   ", "  SSS  "),
+                String.join("", " O   O  ", " O   O  ", "  P      ", "     S "),
+                String.join("", "  OOO   ", "  OOO   ", "  P      ", " SSSS  ")
+        };
 
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
